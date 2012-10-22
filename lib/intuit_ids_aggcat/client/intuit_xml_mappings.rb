9399257e18c4a9d86ec2ee29ad1d2833d5936022
Chris Hart <hartct@gmail.com>
2012-10-21 23:07:23 -0400

    Code cleanup

---
+++ lib/intuit_ids_aggcat/client/intuit_xml_mappings.rb
@@ -0,0 +1,94 @@
+# forward classdefs for mapping
+class Institution; end
+class Address; end
+class Key; end
+class Credentials; end
+class Credential; end
+class Challenge; end
+class ChallengeResponses; end
+
+class Institutions
+  include XML::Mapping
+  array_node :institutions, "institution", :class=>Institution, :default_value => []
+end
+
+class Institution
+  include XML::Mapping
+  text_node :id, "institutionId", :default_value => nil
+  text_node :name, "institutionName", :default_value => nil
+  text_node :url, "homeUrl", :default_value => nil
+  text_node :phone, "phoneNumber", :default_value => nil
+  boolean_node :virtual, "virtual", "true", "false", :default_value => true
+end
+
+class InstitutionDetail
+  include XML::Mapping
+  text_node :id, "institutionId", :default_value => nil
+  text_node :name, "institutionName", :default_value => nil
+  text_node :url, "homeUrl", :default_value => nil
+  text_node :phone, "phoneNumber", :default_value => nil
+  boolean_node :virtual, "virtual", "true", "false", :default_value => true
+  text_node :currency_code, "currencyCode", :default_value => nil
+  text_node :email_address, "emailAddress", :default_value => nil
+  text_node :special_text, "specialText", :default_value => nil
+  object_node :address, "address", :default_value => nil
+  array_node :keys, "keys/key", :class=>Key, :default_value => []
+end
+
+class Address
+  include XML::Mapping
+  text_node :address1, "address1", :default_value => nil
+  text_node :address2, "address2", :default_value => nil
+  text_node :address3, "address3", :default_value => nil
+  text_node :city, "city", :default_value => nil
+  text_node :state, "state", :default_value => nil
+  text_node :postal_code, "postalCode", :default_value => nil
+  text_node :country, "country", :default_value => nil
+end
+
+class Key
+  include XML::Mapping
+  text_node :name, "name", :default_value => nil
+  text_node :status, "status", :default_value => nil
+  text_node :min_length, "valueLengthMin", :default_value => nil
+  text_node :max_length, "valueLengthMax", :default_value => nil
+  boolean_node :is_displayed, "displayFlag", "true", "false", :default_value => true
+  boolean_node :is_masked, "mask", "true", "false", :default_value => false
+  text_node :display_order, "displayOrder", :default_value => nil
+  text_node :instructions, "instructions", :default_value => nil
+  text_node :description, "description", :default_value => nil
+end
+
+class InstitutionLogin
+  include XML::Mapping
+  self.root_element_name "InstitutionLogin"
+  object_node :credentials, "credentials", :default_value => nil
+  object_node :challenge_responses, "challengeResponses", :default_value => nil
+end
+
+class Credentials
+  include XML::Mapping
+  array_node :credential, "credential", :default_value => nil
+end
+
+class Credential
+  include XML::Mapping
+  text_node :name, "name", :default_value => nil
+  text_node :value, "value", :default_value => nil
+end
+
+class Challenges
+  include XML::Mapping
+  object_node :challenge, "challenge", :default_value => nil
+end
+
+class Challenge
+  include XML::Mapping
+  text_node :text, "text", :default_value => nil
+  text_node :image, "image", :default_value => nil
+end
+
+class ChallengeResponses
+  include XML::Mapping
+  text_node :response, "response", :default_value => nil
+end
--- lib/intuit_ids_aggcat/client/services.rb
+++ lib/intuit_ids_aggcat/client/services.rb
@@ -1,23 +1,12 @@
 require 'oauth'
+require 'rexml/document'
 require 'xml/mapping'
+require 'intuit_ids_aggcat/client/intuit_xml_mappings'
+
 module IntuitIdsAggcat
 
   module Client
     
-    # forward classdefs for mapping
-    class Institution; end
-
-    class Institutions
-      include XML::Mapping
-      array_node :institutions, "institution", :class=>Institution, :default_value => []
-    end
-
-    class Institution
-      include XML::Mapping
-      text_node :id, "institutionId", :default_value => nil
-      text_node :name, "institutionName", :default_value => nil
-    end
-
     class Services
 
       class << self
@@ -26,32 +15,42 @@ module IntuitIdsAggcat
           IntuitIdsAggcat::Client::Saml.get_tokens
         end
 
-        def get_institutions(oauth_token_info = IntuitIdsAggcat::Client::Saml.get_tokens, consumer_key = IntuitIdsAggcat.config.oauth_consumer_key, consumer_secret = IntuitIdsAggcat.config.oauth_consumer_secret)
-          # Specify AggCat API endpoint
-  
+        def get_institutions oauth_token_info = IntuitIdsAggcat::Client::Saml.get_tokens, consumer_key = IntuitIdsAggcat.config.oauth_consumer_key, consumer_secret = IntuitIdsAggcat.config.oauth_consumer_secret
+          response_xml = oauth_get_request "https://financialdatafeed.platform.intuit.com/rest-war/v1/institutions", oauth_token_info, consumer_key, consumer_secret
+          institutions = Institutions.load_from_xml(response_xml.root)
+          institutions.institutions
+        end
+
+        def get_institution_detail id, oauth_token_info = IntuitIdsAggcat::Client::Saml.get_tokens, consumer_key = IntuitIdsAggcat.config.oauth_consumer_key, consumer_secret = IntuitIdsAggcat.config.oauth_consumer_secret
+          response_xml = oauth_get_request "https://financialdatafeed.platform.intuit.com/rest-war/v1/institutions/#{id}", oauth_token_info, consumer_key, consumer_secret
+          institutions = InstitutionDetail.load_from_xml(response_xml.root)
+          institutions
+        end
+
+        #def discover_and_add_accounts institution_id, options, oauth_token_info = IntuitIdsAggcat::Client::Saml.get_tokens, consumer_key = IntuitIdsAggcat.config.oauth_consumer_key, consumer_secret = IntuitIdsAggcat.config.oauth_consumer_secret
+        #  url = "https://financialdatafeed.platform.intuit.com/rest-war/v1/institutions/#{institution_id}/logins"
+        #  oauth_token = oauth_token_info[:oauth_token]
+        #  oauth_token_secret = oauth_token_info[:oauth_token_secret]
+
+        #   options = { :request_token_path => 'https://financialdatafeed.platform.intuit.com',
+        #               :timeout => timeout}
+        #   consumer = OAuth::Consumer.new(consumer_key, consumer_secret, options)
+        #   access_token = OAuth::AccessToken.new(consumer, oauth_token, oauth_token_secret)
+        #   xml = 
+        #   response = access_token.post(url, { "Content-Type"=>'application/xml', 'Host' => 'financialdatafeed.platform.intuit.com' })
+        #   response_xml = REXML::Document.new response.body
+        # end
+
+        def oauth_get_request url, oauth_token_info = IntuitIdsAggcat::Client::Saml.get_tokens, consumer_key = IntuitIdsAggcat.config.oauth_consumer_key, consumer_secret = IntuitIdsAggcat.config.oauth_consumer_secret, timeout = 120
           oauth_token = oauth_token_info[:oauth_token]
           oauth_token_secret = oauth_token_info[:oauth_token_secret]
 
-          options = { :request_token_path => 'https://financialdatafeed.platform.intuit.com',
-                      :timeout => 120}
-          # Use your API key and secret to instantiate consumer object
+          options = { :request_token_path => 'https://financialdatafeed.platform.intuit.com', :timeout => timeout} 
           consumer = OAuth::Consumer.new(consumer_key, consumer_secret, options)
-          
-          # # Use your developer token and secret to instantiate access token object
           access_token = OAuth::AccessToken.new(consumer, oauth_token, oauth_token_secret)
-         
-          # Call AggCat service:
-         
-          response = access_token.get("https://financialdatafeed.platform.intuit.com/rest-war/v1/institutions", 
-                 { "Content-Type"=>'application/xml', 'Host' => 'financialdatafeed.platform.intuit.com' })
-          #puts "response: #{response.code}"
-          institutions = Institutions.load_from_xml(response.body)
-          #File.open("institutions.xml", 'w') do |f|
-          #  f.write(response.body)
-          #end
-
+          response = access_token.get(url, { "Content-Type"=>'application/xml', 'Host' => 'financialdatafeed.platform.intuit.com' })
+          response_xml = REXML::Document.new response.body
         end
-
     
       end
     end
