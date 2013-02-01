c405c43b02049dde8fdefdb4e2c82c0ba44d7537
Uri Pomerantz <uri.pomerantz@gmail.com>
2013-01-31 18:18:50 -0800

    testing

--- lib/intuit_ids_aggcat/client/services.rb
+++ lib/intuit_ids_aggcat/client/services.rb
@@ -231,7 +231,7 @@ module IntuitIdsAggcat
           consumer = OAuth::Consumer.new(consumer_key, consumer_secret, options)
           access_token = OAuth::AccessToken.new(consumer, oauth_token, oauth_token_secret)
           begin
-            response = access_token.put(url, nil, { "Content-Type"=>'application/xml', 'Host' => 'financialdatafeed.platform.intuit.com' })
+            response = access_token.put(url, nil, { 'Host' => 'financialdatafeed.platform.intuit.com' })
             return response
             response_xml = REXML::Document.new response.body
           rescue REXML::ParseException => msg
